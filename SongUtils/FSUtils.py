0baaec01eb978dcebec55f17e3b604120e838b03
adenialzz <1160180309@qq.com>
2022-03-11 15:27:01 +0800

    add split_files method in FSUtils

--- SongUtils/FSUtils.py
+++ SongUtils/FSUtils.py
@@ -1,4 +1,8 @@
 import os
+import os.path as osp
+import shutil
+import random
+from tqdm import tqdm
     
 def get_recur_file_list(root):
     def recur(path):
@@ -12,6 +16,21 @@ def get_recur_file_list(root):
     all_file_list = []
     recur(root)
     return all_file_list
+
+
+
+def split_files(all_files_dir, output_dir, num_per_group, shuffle=False):
+    files_list = os.listdir(all_files_dir)
+    if shuffle:
+        random.shuffle(files_list)
+    split_folder_index = 0
+    for i, image in tqdm(enumerate(files_list), total=len(files_list)):
+        if i % num_per_group == 0:
+            split_folder_index += 1
+            currDir = osp.join(output_dir, str(split_folder_index))
+        if not osp.isdir(currDir):
+            os.mkdir(currDir)
+        shutil.copyfile(osp.join(all_files_dir, image), osp.join(currDir, image))
 
 if __name__ == "__main__":
     root = ".."
