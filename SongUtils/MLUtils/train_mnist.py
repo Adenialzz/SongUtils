6297bb49aa80d3239a08a7a01f29a862caf20723
adenialzz <1160180309@qq.com>
2022-03-12 15:57:36 +0800

    add MLUtils

---
+++ SongUtils/MLUtils/train_mnist.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+import sys
+import argparse
+from BaseTrainers import BaseTrainer
+from BaseArgs import Params
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+def main(cfg):
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=cfg.batchSize, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=cfg.batchSize, shuffle=True)
+    loader_list = [train_loader, val_loader]
+    model = Net()
+    mnist_trainer = BaseTrainer(cfg, model, loader_list, metrics_list=['loss', 'acc'])
+    mnist_trainer.forward()
+
+
+if __name__ == "__main__":
+    cfg = Params(sys.argv[1])
+    main(cfg)
